9ae049c1d1ca728b67cd5a5db9bda56ec08b3572
gvharini <harini12ganesan@gmail.com>
2024-10-01 14:28:06 +0530

    recipefinder

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./App.css"
+import { BrowserRouter } from 'react-router-dom'
+import Navbar from './Components/Navbar'
+
+
+const App = () => {
+  return (
+    <BrowserRouter>
+    <Navbar/>
+    </BrowserRouter>
+  
+    
+  )
+}
+
+export default App
