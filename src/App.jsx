39279f4eda9794cb4bbeafa7a378589362540e4d
Lenovo <haris.22aim@kongu.edu>
2024-10-20 21:41:21 +0530

    login page added

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,14 @@ import Category from './Pages/Category/Category'
 import Home from './Pages/Home/Home'
 import Meal from './Pages/SingleMeal/Meal'
 import Navbar from './Components/Navbar'
+import Login from './Pages/Login/login'
 const App = () => {
   return (
     <BrowserRouter>
     <Navbar/>
     <Routes>
-      
-      <Route path='/' element={<Home/>}/>
+    <Route path='/' element={<Login/>}/>
+      <Route path='/Home' element={<Home/>}/>
       <Route path='/menu' element={<Menu/>}/>
       <Route path='category/:category' element={<Category/>}/>
       <Route path='meal/:id' element={<Meal/>}/>
